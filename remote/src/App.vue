c6335a250d75afe08445fc6f82d275dc726d3fcd
Alan Naranjo <85073731+L-A-M-J-O@users.noreply.github.com>
2023-06-19 01:19:47 +0200

    y^CP267CtmN*

---
+++ remote/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="mt-10 text-3xl mx-auto max-w-6xl">
+    <h4>Name: remote</h4>
+    <h5>Framework: vue3</h5>
+    <!-- <Counter /> -->
+  </div>
+</template>
+
+<script setup lang="ts">
+// import Counter from './components/Counter.vue';
+
+</script>
---
+++ remote/src/components/Counter.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const count = ref(0);
+function increment () {
+ count.value++
+}
+</script>
+<template>
+  <section>
+    <h4>{{ count }}</h4>
+    <button class="rounded bg-yellow-600 text-xl text-white " @click="increment">Aumentar</button>
+  </section>
+</template>

8c2bd1a8c4a0d20f381c326fd108301d78887cd8
Alan Naranjo <85073731+L-A-M-J-O@users.noreply.github.com>
2023-06-21 23:27:09 +0200

    creando microfrontends en nuxt 3

---
+++ app-base/app.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Button from './components/Button.vue';
+</script>
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <Button />
+  </div>
+</template>
---
+++ app-base/components/Button.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+const counter = ref(0)
+
+function aumentar () {
+ counter.value++
+}
+</script>
+<template>
+ <section>
+  <div>{{ counter }}</div>
+  <button @click="aumentar">Agregar</button>
+ </section>
+</template>
